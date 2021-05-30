7c78310856576f98340512095569a4ba496a0d5b
will14lee <will14lee@gmail.com>
2021-05-30 02:16:24 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const input = document.getElementById('input');
 function addingEventListener() {
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+  input.addEventListener('click', clickAlert); 
 }
